fbdc33557bd57d6f6ccd154bb853e3643d30e037
Eduardo Marcello <eduardo.duarte@soulasalle.com.br>
2021-05-16 20:51:58 -0300

    Criação do projeto inicial com servlet

---
+++ src/java/servlets/Cadastro.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Cadastro", urlPatterns = {"/Cadastro"})
+public class Cadastro extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String nome = request.getParameter("nome");
+	String email = request.getParameter("email");
+	String password = request.getParameter("password");
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Cadastro concluído!</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Cadastro concluído!</h1>");
+            out.println("<label>Nome: " + nome +"</label><br>");
+            out.println("<label>Email: " + email + "</label><br>");
+            out.println("</body>");
+            out.println("</html>");
+
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
